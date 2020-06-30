6fad089445ef0e32ddaed3dddefe61fd2fe0a18d
Work <rochellecompendio2021@u.northwestern.edu>
2020-06-30 17:22:13 -0500

    added large OR divider

--- src/components/05_Pages/Welcome.js
+++ src/components/05_Pages/Welcome.js
@@ -1,10 +1,23 @@
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 
+// just for the examples, delete later
+import Divider from '../01_Atoms/Divider/Divider.js';
+import BlockButton from '../01_Atoms/Buttons/BlockButton/BlockButton.js';
+
 export default function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Navigation sucks</Text>
+
+      {/* just examples, to be removed later */}
+      <Divider />
+      <BlockButton title="PRIMARY BUTTON" color="primary" />
+      <BlockButton title="SECONDARY BUTTON" color="secondary" />
+      <BlockButton title="FACEBOOK BUTTON" color="facebook" />
+      <BlockButton title="INSTA BUTTON" color="instagram" />
+      {/* ------------------------------------------ */}
+
       <Button
         title="Signup"
         onPress={() => navigation.navigate('Signup')}
--- src/settings/colors.js
+++ src/settings/colors.js
@@ -5,7 +5,7 @@ var colors = {
     highlightColor: '#FF7123', // Off Chance Orange,
 
     lightGray: '#EFEFEF',
-    darkGray: '545454',
+    darkGray: '#545454',
     instaPurple: '#983E9B', // for login/sign in to instagram
     facebookBlue: '#3B5998' // for login/sign in to fb
 };
