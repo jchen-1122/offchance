f45b1e9873162c6f5b0f5f083dc4f7a8a515285a
Work <rochellecompendio2021@u.northwestern.edu>
2020-07-03 19:22:17 -0500

    top 5 donors

--- src/components/04_Templates/GridView/GridView.js
+++ src/components/04_Templates/GridView/GridView.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { View, Text, Footer, FooterTab, Button, Icon } from 'native-base';
 import { styles } from './GridView.styling';
 import { fonts } from '../../../settings/all_settings';
+import GridCard from '../../03_Organisms/GridCard/GridCard';
 
 function GridView(props) {
     return (
       <View style={{backgroundColor: props.bgColor}}>
             <Text style={[fonts.h1, {textAlign: 'center'}]}>{props.title}</Text>
+            <GridCard />
+            {/* <UsernameDisplay /> */}
       </View>
-    )
+    );
 }
 
 export default GridView;
