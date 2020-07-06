8792885aebd18cc6d97a8d504bf4c42f8caaa5be
unknown <jchen1122@berkeley.edu>
2020-07-06 16:35:14 -0700

    timer, urgent color (3 secs or less), auto play and pick, button prompts you to pick or we will choose randomly

--- src/components/05_Pages/Home/Game/Game.js
+++ src/components/05_Pages/Home/Game/Game.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { View, Text, Image, TouchableOpacity } from 'react-native'
 // import {fonts} from '../../../settings/fonts';
 import {colors, fonts, utilities} from '../../../../settings/all_settings';
@@ -7,13 +7,35 @@ import GameBar from '../../../02_Molecules/GameBar/GameBar'
 import BlockButton from '../../../01_Atoms/Buttons/BlockButton/BlockButton'
 import {styles} from './Game.styling'
 
-function Game(props,{navigation}) {
-    let choices = ['rock', 'paper', 'scissors']
-    let compChoice = choices[Math.floor(Math.random()*3)] // randomly pick rock, paper, or scissors for computer
+function Game(props) {
+    // let choices = ['rock', 'paper', 'scissors']
+    // let compChoice = choices[Math.floor(Math.random()*3)] // randomly pick rock, paper, or scissors for computer
     let localWins = 0
     let localChances = 0
     let localRounds = 0
-    //console.warn(compChoice)
+    
+    const [localTime, localSetTime] = useState(10)
+
+    useEffect(() => {
+        let interval = null
+        if (localTime > 0) {
+            interval = setInterval(() => {
+                localSetTime(localTime => localTime - 1)
+            }, 1000)
+        }
+        return () => {
+            if (localTime === 1) {
+                props.setPage('PlayGame')
+                props.setRound(props.round + localRounds)
+                props.setWins(props.wins + localWins)
+                props.setChances(props.chances + localChances)
+                props.setOpacity([1,1,1])
+                props.setChoice("Pick Rock Paper or Scissors")
+                props.setTime(10)
+            }
+            clearInterval(interval)
+        }
+    }, [localTime])
 
     // map players choice to image on UI
     let playerChoiceImg;
@@ -31,7 +53,7 @@ function Game(props,{navigation}) {
 
     // map computers choice to image on UI
     let compChoiceImg;
-    switch (compChoice){
+    switch (props.compChoice){
         case 'rock':
             compChoiceImg = <Image style={styles.compChoice} source={require('../../../../../assets/game/rockFlip.png')} />
             break;
@@ -48,37 +70,40 @@ function Game(props,{navigation}) {
     let message;
     let messageStyles = [styles.message]
     // determines who wins
-    if (props.choice == compChoice){
+    if (props.choice == props.compChoice){
         winner = 'tie'
         message = "It's a tie!"
         messageStyles.push(styles.message_tie)
     }
-    else if ((props.choice == 'rock' && compChoice == 'scissors') || (props.choice == 'paper' && compChoice == 'rock') || (props.choice == 'scissors' && compChoice == 'paper')){
+    else if ((props.choice == 'rock' && props.compChoice == 'scissors') || (props.choice == 'paper' && props.compChoice == 'rock') || (props.choice == 'scissors' && props.compChoice == 'paper')){
         winner = 'player'
         message = "You win this round!"
         messageStyles.push(styles.message_win)
         localWins += 1
+        localChances -= 1
+        localRounds += 1
     }
     else{
         winner = 'computer'
         message = "You lost this round :("
         messageStyles.push(styles.message_lose)
         localChances -= 1
+        localRounds += 1
     }
 
     return (
         <View style={utilities.container}>
             <View style={[utilities.container], {backgroundColor: 'black', height: '50%'}}>
-                <GameBar color={'white'} currRound={props.round} chancesLeft={props.chances + localChances} wins={props.wins + localWins} numRounds={10}></GameBar>
+                <GameBar color={'white'} currRound={props.round + localRounds} chancesLeft={props.chances + localChances} wins={props.wins + localWins} numRounds={10}></GameBar>
                 <View style={{flex: 0, alignItems: 'center'}}>
-                    <Text style={{color: 'white'}}>{compChoice}</Text>
+                    {/* <Text style={{color: 'white'}}>{compChoice}</Text> */}
                     {compChoiceImg}
                 </View>
             </View>
             
             <View style={utilities.container}>
                 <View style={{flex: 0, alignItems: 'center'}}>
-                    <Text style={{color: 'black'}}>{props.choice}</Text>
+                    {/* <Text style={{color: 'black'}}>{props.choice}</Text> */}
                     {playerChoiceImg}
                 </View>
                 <Text style={messageStyles}>{message}</Text>
@@ -89,13 +114,17 @@ function Game(props,{navigation}) {
                         size="short"
                         onPress={() => {
                             props.setPage('PlayGame')
+                            props.setRound(props.round + localRounds)
                             props.setWins(props.wins + localWins)
                             props.setChances(props.chances + localChances)
+                            props.setOpacity([1,1,1])
+                            props.setChoice("Pick Rock Paper or Scissors")
+                            props.setTime(10)
                         }} />
                         {/* onPress={() => navigation.navigate('PlayGame')}></BlockButton> */}
                 </View>
             </View>
-            <BottomNav navigation={navigation} active={'Home'}></BottomNav>
+            <BottomNav navigation={props.navigation} active={'Home'}></BottomNav>
         </View>
     )
 }
--- src/components/GameController.js
+++ src/components/GameController.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import PlayGame from './05_Pages/Home/PlayGame/PlayGame';
 import Game from './05_Pages/Home/Game/Game';
+import {Text} from 'react-native'
 import { set } from 'react-native-reanimated';
 
 function GameController({navigation}) {
@@ -8,22 +9,20 @@ function GameController({navigation}) {
     const [round, setRound] = useState(1)
     const [chances, setChances] = useState(35)
     const [wins, setWins] = useState(0)
-
-    const [choice, setChoice] = useState(null)
-    // console.log('page; ' + page)
-    // console.log('round: ' + round)
-    // console.log('chances: ' + chances)
-    // console.log('wins: ' + wins)
-    // console.log('choice: ' +  choice)
-    // console.log('-------------------------------------------------')
+    const [choice, setChoice] = useState("Pick Rock Paper or Scissors")
+    const [opacity, setOpacity] = useState([1, 1, 1])
+    const [time, setTime] = useState(10)
+    
+    let choices = ['rock', 'paper', 'scissors']
+    let compChoice = choices[Math.floor(Math.random()*3)] // randomly pick rock, paper, or scissors for computer
 
     if (page == 'PlayGame'){
         return (
-            <PlayGame setPage={setPage} round={round} setRound={setRound} chances={chances} setChances={setChances} wins={wins} setWins={setWins} setChoice={setChoice}/>
+            <PlayGame navigation={navigation} time={time} setTime={setTime} opacity={opacity} setOpacity={setOpacity} setPage={setPage} round={round} setRound={setRound} chances={chances} setChances={setChances} wins={wins} setWins={setWins} setChoice={setChoice} choice={choice}/>
         )
     }
     else if (page == 'Game'){
-        return <Game setPage={setPage} round={round} setRound={setRound} chances={chances} setChances={setChances} wins={wins} setWins={setWins} choice={choice}/>
+        return <Game navigation={navigation} time={time} compChoice={compChoice} setTime={setTime} opacity={opacity} setOpacity={setOpacity} setPage={setPage} round={round} setRound={setRound} chances={chances} setChances={setChances} wins={wins} setWins={setWins} setChoice={setChoice} choice={choice}/>
     }
 }
 
