a36f4ad1c25364713b307ab4ca7849914750fa28
Work <rochellecompendio2021@u.northwestern.edu>
2020-07-09 10:43:47 -0500

    ? merge conflicts

---
+++ src/components/05_Pages/Profile/Profile.styling.js
@@ -0,0 +1,47 @@
+import {StyleSheet, Dimensions} from 'react-native';
+import {fonts} from "../../../settings/all_settings";
+
+const styles = StyleSheet.create({
+    profilePic: {
+        height: normalSize,
+        width: normalSize,
+        borderRadius: normalSize,
+        margin: 5
+    },
+
+    // different sizes for profile pic
+    profilePic_large: {
+        height: largeSize,
+        width: largeSize,
+        borderRadius: largeSize,
+        margin: 15
+    },
+    profilePic_small: {
+        height: smallSize,
+        width: smallSize,
+        borderRadius: smallSize,
+        marginTop: 5,
+        marginBottom: 5,
+        marginRight: 5
+    },
+    username_large: {
+        fontSize: 18
+    },
+    username_small: {
+        fontSize: 11.5
+    },
+
+    container: {
+        flex: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    container_hostedBy: {
+        marginRight: 'auto',
+        flex: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+})
+
+export {styles};
