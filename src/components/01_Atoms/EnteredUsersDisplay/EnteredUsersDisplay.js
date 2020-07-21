4401be244bdd630f45387c9de368894da3feca21
Work <rochellecompendio2021@u.northwestern.edu>
2020-07-21 15:38:36 -0500

    fuck me up

--- src/components/01_Atoms/EnteredUsersDisplay/EnteredUsersDisplay.js
+++ src/components/01_Atoms/EnteredUsersDisplay/EnteredUsersDisplay.js
@@ -7,35 +7,38 @@ import GlobalState from '../../globalState'
 function EnteredUsersDisplay(props) {
     const ip = require('../../IP_ADDRESS.json')
     const { user, setUser } = useContext(GlobalState)
-    const [ countMsg, setCountMsg ] = useState("No entries yet")
-    const [ image1, setImage1 ] = useState('') // uri for first profile image
-    const [ image2, setImage2 ] = useState('') // uri for second profile image
-    const [ user1, setUser1] = useState(null)
+    const [countMsg, setCountMsg] = useState("No entries yet")
+    const [image1, setImage1] = useState('') // uri for first profile image
+    const [image2, setImage2] = useState('') // uri for second profile image
+    const [user1, setUser1] = useState(null)
     var enteredUsers;
 
-    React.useEffect(() => {
-        // sort entered users so the people you're following show up at the front
-        const sortUsers = (users) => {
-            var entered = []
-            for (let i = 0; i < users.length; i++) {
-                let enteredUser = users[i]
-                // if you're following them, add to the top
-                if (user.following.includes(enteredUser.userID)) {
-                    entered.unshift(enteredUser)
-                }
-                // if you're not following them, push to back
-                else {
-                    entered.push(enteredUser)
-                }
-            }
-            return entered
+    // sort entered users so the people you're following show up at the front
+    const sortUsers = (users) => {
+        if (Object.keys(user).length === 0 && user.constructor === Object) {
+            return users
         }
+        var entered = []
+        for (let i = 0; i < users.length; i++) {
+            let enteredUser = users[i]
+            // if you're following them, add to the top
+            if (user.following.includes(enteredUser.userID)) {
+                entered.unshift(enteredUser)
+            }
+            // if you're not following them, push to back
+            else {
+                entered.push(enteredUser)
+            }
+        }
+        return entered
+    }
 
+    React.useEffect(() => {
         // gets prof picture and username from db given certain use id
         const getUserInfo = async (userID, field) => {
             let response = await fetch('http://' + ip.ipAddress + ':3000/user/id/' + userID)
             response = await response.json()
-            if (field == 'profPic'){
+            if (field == 'profPic') {
                 return response.profilePicture
             }
             else {
@@ -45,7 +48,7 @@ function EnteredUsersDisplay(props) {
 
         // sets uri of the two profile pictures to the first two people in the list
         // also sets name of first user you see on card
-        const renderImages = async(users) => {
+        const renderImages = async (users) => {
             if (users.length > 0) {
                 setImage1(await getUserInfo(enteredUsers[0].userID, 'profPic'))
                 setUser1(await getUserInfo(enteredUsers[0].userID, 'username'))
@@ -55,22 +58,24 @@ function EnteredUsersDisplay(props) {
             }
         }
 
-        if (props.enteredUsers) {
-            enteredUsers = sortUsers(props.enteredUsers)
-            renderImages(enteredUsers)
-            // changes display text if theres entered users
-            if (enteredUsers.length > 0){
-                setCountMsg("Entered by "+user1+" and " + enteredUsers.length.toString() + " others")
+        const renderInfo = async () => {
+            if (props.enteredUsers) {
+                enteredUsers = sortUsers(props.enteredUsers)
+                await renderImages(enteredUsers)
+                // changes display text if theres entered users
+                if (enteredUsers.length > 0) {
+                    setCountMsg("Entered by " + user1 + " and " + enteredUsers.length.toString() + " others")
+                }
             }
-            
         }
-      }, [])
+        renderInfo()
+    }, [user1])
 
     return (
         <TouchableOpacity onPress={() => props.navigation.navigate('EnteredUsers')}>
             <View style={styles.container}>
-                {image1 != '' ? <Image style={[styles.image, styles.image_overlapped]} source={{ uri: image1}} /> : null}
-                {image2 != '' ? <Image style={[styles.image,{marginRight: 5}]} source={{ uri: image2 }} /> : null}
+                {image1 != '' ? <Image style={[styles.image, styles.image_overlapped]} source={{ uri: image1 }} /> : null}
+                {image2 != '' ? <Image style={[styles.image, { marginRight: 5 }]} source={{ uri: image2 }} /> : null}
                 <Text style={fonts.p}>{countMsg}</Text>
             </View>
         </TouchableOpacity>
