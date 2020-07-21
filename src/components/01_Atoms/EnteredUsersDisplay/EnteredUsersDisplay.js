8cfa4a49ad2b03d3366970158adb6c1b20b4852a
Work <rochellecompendio2021@u.northwestern.edu>
2020-07-21 14:47:48 -0500

    entered users on card is done

--- src/components/01_Atoms/EnteredUsersDisplay/EnteredUsersDisplay.js
+++ src/components/01_Atoms/EnteredUsersDisplay/EnteredUsersDisplay.js
@@ -1,39 +1,76 @@
-import React, {useState, useContext, useEffect} from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { TouchableOpacity, Text, Image, View, ScrollView } from 'react-native';
 import { fonts, utilities } from '../../../settings/all_settings';
 import styles from './EnteredUsersDisplay.styling';
 import GlobalState from '../../globalState'
 
 function EnteredUsersDisplay(props) {
-    const {user, setUser} = useContext(GlobalState)
-    var enteredUsers = []
+    const ip = require('../../IP_ADDRESS.json')
+    const { user, setUser } = useContext(GlobalState)
+    const [ countMsg, setCountMsg ] = useState("No entries yet")
+    const [ image1, setImage1 ] = useState('') // uri for first profile image
+    const [ image2, setImage2 ] = useState('') // uri for second profile image
+    const [ user1, setUser1] = useState(null)
+    var enteredUsers;
 
-    if (props.enteredUsers) {
+    React.useEffect(() => {
         // sort entered users so the people you're following show up at the front
-        for (let i = 0; i < props.enteredUsers.length; i++) {
-            let enteredUser = props.enteredUsers[i]
-            // if you're following them, add to the top
-            if (user.following.includes(enteredUser.userID)){
-                enteredUsers.unshift(enteredUser)
+        const sortUsers = (users) => {
+            var entered = []
+            for (let i = 0; i < users.length; i++) {
+                let enteredUser = users[i]
+                // if you're following them, add to the top
+                if (user.following.includes(enteredUser.userID)) {
+                    entered.unshift(enteredUser)
+                }
+                // if you're not following them, push to back
+                else {
+                    entered.push(enteredUser)
+                }
             }
-            // if you're not following them, push to back
-            else{
-                enteredUsers.push(enteredUser)
+            return entered
+        }
+
+        // gets prof pictures from db given certain use id
+        const getUserInfo = async (userID, field) => {
+            let response = await fetch('http://' + ip.ipAddress + ':3000/user/id/' + userID)
+            response = await response.json()
+            if (field == 'profPic'){
+                return response.profilePicture
+            }
+            else {
+                return response.username
             }
         }
-    }
 
-    // conditionally display text if there's entries
-    let enteredUsersCount = "No entries yet"
-    if (props.enteredUsers && props.enteredUsers.length > 0) {
-        enteredUsersCount = "Entered by user 1 and " + props.enteredUsers.length.toString() + " others"
-    }
+        // sets uri of the two profile pictures to the first two people in the list
+        const renderImages = async(users) => {
+            if (users.length > 0) {
+                setImage1(await getUserInfo(enteredUsers[0].userID, 'profPic'))
+                setUser1(await getUserInfo(enteredUsers[0].userID, 'username'))
+            }
+            if (users.length > 1) {
+                setImage2(await getUserInfo(enteredUsers[1].userID, 'profPic'))
+            }
+        }
+
+        if (props.enteredUsers) {
+            enteredUsers = sortUsers(props.enteredUsers)
+            renderImages(enteredUsers)
+            // changes display text if theres entered users
+            if (enteredUsers.length > 0){
+                setCountMsg("Entered by "+user1+" and " + enteredUsers.length.toString() + " others")
+            }
+            
+        }
+      }, [])
+
     return (
         <TouchableOpacity onPress={() => props.navigation.navigate('EnteredUsers')}>
             <View style={styles.container}>
-                <Image style={[styles.image, styles.image_overlapped]} source={{ uri: "https://i.pinimg.com/originals/dc/24/88/dc2488feb2d6dc4750a95a1f715c67d8.jpg" }} />
-                <Image style={styles.image} source={{ uri: "https://vignette.wikia.nocookie.net/avatar/images/7/7a/Katara_smiles_at_coronation.png/revision/latest?cb=20150104171449" }} />
-                <Text style={[fonts.p, { marginLeft: 5 }]}>{enteredUsersCount}</Text>
+                {image1 != '' ? <Image style={[styles.image, styles.image_overlapped]} source={{ uri: image1}} /> : null}
+                {image2 != '' ? <Image style={[styles.image,{marginRight: 5}]} source={{ uri: image2 }} /> : null}
+                <Text style={fonts.p}>{countMsg}</Text>
             </View>
         </TouchableOpacity>
     )
