72f9ddc8768414d4de572773626d2ce5434f5c3d
Work <rochellecompendio2021@u.northwestern.edu>
2020-07-22 18:08:54 -0500

    not logged in

--- src/components/02_Molecules/BottomNav/BottomNav.js
+++ src/components/02_Molecules/BottomNav/BottomNav.js
@@ -1,8 +1,11 @@
-import React, {useState} from 'react'
+import React, { useContext } from 'react'
 import { View, Text, Footer, FooterTab, Button, Icon } from 'native-base';
 import {colors} from '../../../settings/colors'
+import {user_logged_in} from '../../../functions/user_functions'
+import GlobalState from '../../globalState';
 
 function BottomNav(props) {
+    const {user, setUser} = useContext(GlobalState)
     return (
       <View>
         <Footer>
@@ -19,7 +22,7 @@ function BottomNav(props) {
               <Icon name="card" style={(props.active === 'Likes') ? {color:'white'} : null}/>
               <Text style={(props.active === 'Likes') ? {color:'white'} : null}>Likes</Text>
             </Button>
-            <Button onPress={() => props.navigation.navigate('Account')}>
+            <Button onPress={() => (user_logged_in(user)) ? props.navigation.navigate('Account') : props.navigation.navigate('NotLogin')}>
               <Icon name="person" style={(props.active === 'Account') ? {color:'white'} : null}/>
               <Text style={(props.active === 'Account') ? {color:'white'} : null}>Account</Text>
             </Button>
