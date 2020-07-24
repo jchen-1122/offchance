7f06786c40fff2101e059c69c3e1c755ad1e2c0a
Work <rochellecompendio2021@u.northwestern.edu>
2020-07-24 15:51:50 -0500

    center text

--- src/components/05_Pages/Welcome/Welcome.js
+++ src/components/05_Pages/Welcome/Welcome.js
@@ -36,8 +36,8 @@ export default function HomeScreen({ navigation }) {
 
       {/* TODO: image should be aligned closer to the top */}
       <Image style={styles.logo} source={{ uri: 'https://oc-mobile-images.s3.us-east.cloud-object-storage.appdomain.cloud/oc-logo.png' }} />
-      <Text style={fonts.h1}>Daily Live Drawings for the Hottest Sneakers and Collectables</Text>
-      <Text style={[fonts.h3, { fontWeight: 'normal', width: '88%', marginTop: '2%' }]}>Donate to Important Causes and Win</Text>
+      <Text style={[fonts.h1, {textAlign: 'center'}]}>Daily Live Drawings for the Hottest Sneakers and Collectables</Text>
+      <Text style={[fonts.h3, { fontWeight: 'normal'}]}>Donate to Important Causes and Win</Text>
 
       <Image style={styles.welcomeImage} source={{ uri: 'https://oc-mobile-images.s3.us-east.cloud-object-storage.appdomain.cloud/welcome_image.png' }} />
       {/* Links to Signup */}
