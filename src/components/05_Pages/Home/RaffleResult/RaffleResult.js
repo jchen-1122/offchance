699bc68c0f895c6dd765255c6481fb32176ce8fb
zhangtia <zhangtia@umich.edu>
2020-07-27 15:38:40 -0400

    todo: gold gradient

--- src/components/05_Pages/Home/RaffleResult/RaffleResult.js
+++ src/components/05_Pages/Home/RaffleResult/RaffleResult.js
@@ -1,26 +1,29 @@
 import React, {useState, useEffect} from 'react';
-import { ScrollView, View, Text, Image, Dimensions, TouchableOpacity } from 'react-native'
+import { ScrollView, View, Text, Image, Dimensions, TouchableOpacity } from 'react-native';
 import { Overlay } from 'react-native-elements';
-import {utilities, fonts, colors} from '../../../../settings/all_settings';
 import styles from './RaffleResult.styling';
-import TextLink from '../../../01_Atoms/Buttons/TextLinks/TextLinks'
-import BottomNav from '../../../02_Molecules/BottomNav/BottomNav'
-import ProgressBar from '../../../02_Molecules/ProgressBar/ProgressBar'
-import HostedBy from '../../../02_Molecules/HostedBy/HostedBy'
-import Top5Donors from '../../../02_Molecules/Top5Donors/Top5Donors'
-import DropDown from '../../../01_Atoms/DropDown/DropDown'
-import ImageCarousel from '../../../02_Molecules/ImageCarousel/ImageCarousel'
-import BlockButton from '../../../01_Atoms/Buttons/BlockButton/BlockButton';
-import { get_user } from '../../../fake_users/stub-users.js';
+import TextLink from '../../../01_Atoms/Buttons/TextLinks/TextLinks';
+import LinearGradient from 'react-native-linear-gradient';
+
 
 
 export default function RaffleResult({navigation, route}) {
     const [selected, setSelected] = useState(null)
     const [overlay, setoverlay] = useState(false)
     const [prize, setPrize] = useState(null)
-    const enteredUsers = route.params.raffle.users.children
+    const [enteredUsers, setEnteredUsers] = useState([])
+    const [winners, setWinners] = useState({})
+    const ip = require('../../../IP_ADDRESS.json');
 
-    const winners = {}
+
+    React.useEffect(() => {
+        async function getRaffle(id) {
+            let response = await fetch('http://' + ip.ipAddress + ':3000/raffle/id/' + id)
+            response = await response.json()
+            setEnteredUsers(response.users.children)
+        }
+        getRaffle(route.params.raffle._id)
+    }, [])
     
     // will change based on the number of rewards
     // rewards[0] = grand prize (1)
@@ -34,63 +37,101 @@ export default function RaffleResult({navigation, route}) {
     // const winners = route.params.raffle.users.children.sort((a,b)=>b.amountDonated - a.amountDonated).slice(0,numWinners)
 
     // randomly and proportionally assign rewards to users
+    React.useEffect(() => {
 
-    while (numRewards !== 0) {
-        // 1. assign everyone a range of numbers based on the number of chances
-        let ranges = {}
-        let count = 1
-        let numChances = 0
-        for (var i = 0; i < enteredUsers.length; i++) {
-            ranges[enteredUsers[i].userID] = [count, count + enteredUsers[i].chances - 1]
-            count += enteredUsers[i].chances
-            numChances += enteredUsers[i].chances
-        }
+        let temp = {}
 
-        // 2. Generate a random number from 0 to numChances
-        const rand = Math.floor((Math.random() * numChances) + 1)
+        while (numRewards !== 0) {
+            // 1. assign everyone a range of numbers based on the number of chances
+            let ranges = {}
+            let count = 1
+            let numChances = 0
+            for (var i = 0; i < enteredUsers.length; i++) {
+                ranges[enteredUsers[i].userID] = [count, count + enteredUsers[i].chances - 1]
+                count += enteredUsers[i].chances
+                numChances += enteredUsers[i].chances
+            }
 
-        // 3. determine who's range qualifies (both ends inclusive)
-        let winner = -1
-        for (var i = 0; i < enteredUsers.length; i++) {
-            if (ranges[enteredUsers[i].userID][0] <= rand && ranges[enteredUsers[i].userID][1] >= rand) {
-                winner = enteredUsers[i].userID
-                winners[enteredUsers[i].userID] = currPrize
-                break
+            // 2. Generate a random number from 0 to numChances
+            const rand = Math.floor((Math.random() * numChances) + 1)
+
+            // 3. determine who's range qualifies (both ends inclusive)
+            let winner = -1
+            for (var i = 0; i < enteredUsers.length; i++) {
+                if (ranges[enteredUsers[i].userID][0] <= rand && ranges[enteredUsers[i].userID][1] >= rand) {
+                    winner = enteredUsers[i].userID
+                    temp[enteredUsers[i].userID] = currPrize
+                    break
+                }
+            }
+
+            // 4. update variables for next loop
+            numRewards--;
+            rewards[currPrize] -= 1
+            if (rewards[currPrize] == 0) {
+                currPrize += 1
+            }
+
+            // 5. delete current winner from array
+            for(var i = enteredUsers.length - 1; i >= 0; i--) {
+                if(enteredUsers[i].userID === winner) {
+                    // console.log('deleted')
+                    enteredUsers.splice(i, 1);
+                    break
+                }
             }
         }
+        setWinners(temp)
 
-        // 4. update variables for next loop
-        numRewards--;
-        rewards[currPrize] -= 1
-        if (rewards[currPrize] == 0) {
-            currPrize += 1
-        }
-
-        // 5. delete current winner from array
-        for(var i = enteredUsers.length - 1; i >= 0; i--) {
-            if(enteredUsers[i].userID === winner) {
-                // console.log('deleted')
-                enteredUsers.splice(i, 1);
-                break
-            }
-        }
-    }
-
-    // @Matt the winners object is {key: userId, value: reward}, reward rn is just a number (see top of file)
-    console.log(winners)
+        // @Matt the winners object is {key: userId, value: reward}, reward rn is just a number (see top of file)
+        console.log(winners)
+    }, [enteredUsers])
 
     //const customData = require('../../../fake_users/stub-users.json');
     //let plswork = customData.users[2].name
 
-    let CardArray = Array(100).fill().map((_, i) => <TouchableOpacity style={styles.card} 
-        onPress={() => {
-            setoverlay(true)
-            setSelected(get_user(i+1).name)
-            setPrize(get_user(i+1).prize)
-        }}>
-            <Text>{i + 1}</Text>
-            {/* This is what will be displayed on the back of the cards */}
-        </TouchableOpacity>);
+    let CardArray = []
+    let count = 0
+
+    for (var key in winners) {
+        //console.log('1')
+        // check if the property/key is defined in the object itself, not in parent
+        if (winners.hasOwnProperty(key)) {    
+            var color
+            count++
+            switch (winners[key]) {
+                case 0:
+                    color = styles.gold;
+                    break;
+                case 1:
+                    color = styles.silver;
+                    break;
+                case 2:
+                    color = styles.orange;
+                    break;
+                default:
+                    color = styles.purple;
+                    break;
+            }
+            CardArray.push(
+                <TouchableOpacity style={[styles.card, color]} 
+                    onPress={() => {
+                        setoverlay(true)
+                        setSelected(count)
+                        setPrize(winners[key])
+                    }}>
+                        <View style={styles.circle_outline} >
+                            <Image 
+                                style={styles.circle_pic}
+                                source={{ uri: 'https://oc-mobile-images.s3.us-east.cloud-object-storage.appdomain.cloud/oc-logo.png' }}
+                            />
+                        </View>
+        
+                        {/* This is what will be displayed on the back of the cards */}
+                </TouchableOpacity>
+            )
+        }
+    }
 
 
     return (
@@ -99,6 +140,11 @@ export default function RaffleResult({navigation, route}) {
                 {CardArray}
                 <Overlay isVisible={overlay} onBackdropPress={() => setoverlay(false)}>
                     <View style={styles.overlay}>
+                    <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']}>
+                        <Text>
+                            Sign in with Facebook
+                        </Text>
+                    </LinearGradient>
                         {/* This is what is displayed within the overlay */}
                         <TextLink
                         title={selected}
