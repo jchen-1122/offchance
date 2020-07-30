b6e0f1058682def12e3397febc4ad9c174a6f0bf
zhangtia <zhangtia@umich.edu>
2020-07-30 18:10:07 -0400

    raffles display each winner now

--- src/components/05_Pages/Home/RaffleResult/RaffleResult.js
+++ src/components/05_Pages/Home/RaffleResult/RaffleResult.js
@@ -26,7 +26,7 @@ export default function RaffleResult({ navigation, route }) {
     const {user, setUser} = useContext(GlobalState)
 
     // test user and raffle for Chelly's card
-    let dummy_user = get_user("Chelly")
+    //let dummy_user = get_user("Chelly")
 
 
     React.useEffect(() => {
@@ -100,7 +100,7 @@ export default function RaffleResult({ navigation, route }) {
                         <TouchableOpacity style={[styles.card]}
                             onPress={() => {
                                 setoverlay(true)
-                                setSelected(count)
+                                setSelected(element)
                                 setPrize(element["prize"])
                             }}>
     
@@ -121,159 +121,13 @@ export default function RaffleResult({ navigation, route }) {
     }, [winners])
 
 
-
-    // will change based on the number of rewards
-    // rewards[0] = grand prize (1)
-    // rewards[1] = 50 chances (2)
-    // rewards[2] = 20 chanes (3)
-    // rewards[3] = 10 chances (4)
-    //let rewards = [1, 2, 3, 4]
-    //let numRewards = 10
-    //let currPrize = 0
-
-
-    // // const winners = route.params.raffle.users.children.sort((a,b)=>b.amountDonated - a.amountDonated).slice(0,numWinners)
-
-    // randomly and proportionally assign rewards to users
-    /*React.useEffect(() => {
-
-        let temp = {}
-
-        while (numRewards !== 0) {
-            // 1. assign everyone a range of numbers based on the number of chances
-            let ranges = {}
-            let count = 1
-            let numChances = 0
-            for (var i = 0; i < enteredUsers.length; i++) {
-                ranges[enteredUsers[i].userID] = [count, count + enteredUsers[i].chances - 1]
-                count += enteredUsers[i].chances
-                numChances += enteredUsers[i].chances
-            }
-
-            // 2. Generate a random number from 0 to numChances
-            const rand = Math.floor((Math.random() * numChances) + 1)
-
-            // 3. determine who's range qualifies (both ends inclusive)
-            let winner = -1
-            for (var i = 0; i < enteredUsers.length; i++) {
-                if (ranges[enteredUsers[i].userID][0] <= rand && ranges[enteredUsers[i].userID][1] >= rand) {
-                    winner = enteredUsers[i].userID
-                    temp[enteredUsers[i].userID] = currPrize
-                    break
-                }
-            }
-
-            // 4. update variables for next loop
-            numRewards--;
-            rewards[currPrize] -= 1
-            if (rewards[currPrize] == 0) {
-                currPrize += 1
-            }
-
-            // 5. delete current winner from array
-            for (var i = enteredUsers.length - 1; i >= 0; i--) {
-                if (enteredUsers[i].userID === winner) {
-                    enteredUsers.splice(i, 1);
-                    break
-                }
-            }
-        }
-
-        async function getWinnerObjs(ids) {
-            // get top 5 donors of this raffle
-            let temp = []
-            for (var i = 0; i < ids.length; i++) {
-                console.log('i',i)
-                const user = await fetch('http://' + ip.ipAddress + '/user/id/' + ids[i])
-                user = await user.json()
-                console.log('user',user)
-                temp.push(user)
-            }
-            // console.log('temp', temp)
-            setWinnerObjs(temp)
-        }
-        getWinnerObjs(Object.keys(temp))
-        setWinners(temp)
-
-        async function matttest() {
-            const winnerRes = await fetch('http://'+ip.ipAddress+'/user/ids/' ,{
-                method: "PATCH",
-                headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-                },  
-                body: winnerJSON()
-            })
-            const matt = await winnerRes.json()
-            //console.log(matt)
-        }
-        const winnerJSON = () => {
-            let data = {
-                ids: Object.keys(temp)
-            }
-            return JSON.stringify(data)
-        }
-
-        matttest()
-
-        // @Matt the winners object is {key: userId, value: reward}, reward rn is just a number (see top of file)
-        
-    }, [enteredUsers])
-    //console.log('winners',winners)
-    //console.log('winnerObjs', winnerObjs)
-    //const customData = require('../../../fake_users/stub-users.json');
-    //let plswork = customData.users[2].name
-
-
-
-    for (let key in winners) {
-        // check if the property/key is defined in the object itself, not in parent
-        if (winners.hasOwnProperty(key)) {
-            var gradient;
-            count++
-            switch (winners[key]) {
-                case 0:
-                    gradient = colors.goldGradientBg
-                    break;
-                case 1:
-                    gradient = colors.silverGradientBg
-                    break;
-                case 2:
-                    gradient = colors.bronzeGradientBg
-                    break;
-                default:
-                    gradient = [colors.blue, colors.blue]
-                    break;
-            }
-            CardArray.push(
-                <LinearGradient start={[0, 0]} end={[1, 0]} colors={gradient} style={{margin: Dimensions.get('window').width * 0.005}}>
-                    <TouchableOpacity style={[styles.card]}
-                        onPress={() => {
-                            setoverlay(true)
-                            setSelected(count)
-                            setPrize(winners[key])
-                        }}>
-
-                        <View style={styles.circle_outline} >
-                            <Image
-                                style={styles.circle_pic}
-                                source={{ uri: 'https://oc-mobile-images.s3.us-east.cloud-object-storage.appdomain.cloud/oc-logo.png' }}
-                            />
-                        </View>
-
-                    </TouchableOpacity>
-                </LinearGradient>
-            )
-        }
-    }*/
-
     return (
         <View>
         <ScrollView>
             <View style={styles.container}>
                 {display}
                 <Overlay isVisible={overlay} onBackdropPress={() => setoverlay(false)} overlayStyle={{ backgroundColor: 'transparent' }}>
-                    <WinnerCard prize={prize} winner={dummy_user} dummy_winner={dummy_user} raffle={raffle} host={host} selected={selected} navigation={navigation} />
+                    <WinnerCard prize={prize} winner={selected} raffle={raffle} host={host} navigation={navigation} />
                 </Overlay>
             </View>
         </ScrollView>
