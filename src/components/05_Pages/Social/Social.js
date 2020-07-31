d7e11006984b80725321b016a93f85a1f559ff2b
unknown <jchen1122@berkeley.edu>
2020-07-30 17:08:35 -0700

    stylesheet

--- src/components/05_Pages/Social/Social.js
+++ src/components/05_Pages/Social/Social.js
@@ -4,6 +4,7 @@ import {Icon} from 'react-native-elements'
 import BottomNav from '../../02_Molecules/BottomNav/BottomNav'
 import io from "socket.io-client";
 import { min } from "react-native-reanimated";
+import {styles} from './Social.styling'
 
 export default class Social extends Component {
   constructor(props) {
@@ -32,27 +33,27 @@ export default class Social extends Component {
 
   render() {
     const chatMessages = this.state.chatMessages.map(chatMessage => (
-      <View style={{flexDirection: 'row', marginBottom: Dimensions.get('screen').height * 0.02, backgroundColor: 'rgba(255,250,250,0.7)', borderRadius: '22/2', padding: '2%', paddingBottom: '1%', maxWidth: Dimensions.get('screen').width * 0.82, width: Math.max(chatMessage.message.length * 11, chatMessage.username.length * 13)}}>
-        <Image source={{uri: chatMessage.profilePicture}} style={{width: 40, height: 40, borderRadius: 40/2 }}></Image>
+      <View style={{flexDirection: 'row', marginBottom: Dimensions.get('screen').height * 0.02, backgroundColor: 'rgba(255,250,250,0.7)', borderRadius: '22/2', padding: '2%', paddingBottom: '1%', maxWidth: Dimensions.get('screen').width * 0.82, width: Math.max(chatMessage.message.length * 11, (chatMessage.username.length+2) * 13)}}>
+        <Image source={{uri: chatMessage.profilePicture}} style={styles.proPic}></Image>
         <View style={{marginLeft: Dimensions.get('screen').width * 0.01, marginRight: (chatMessage.message.length * 11 < Dimensions.get('screen').width * 0.82) ? 0 : Dimensions.get('screen').width * 0.13}}>
-          <Text style={{fontSize: 14, color: 'black', fontWeight: '700'}}>@{chatMessage.username}</Text>
-          <Text style={{fontSize: 14, color: 'black', fontWeight: '600'}}>{chatMessage.message}</Text>
+          <Text style={styles.usn}>@{chatMessage.username}</Text>
+          <Text style={styles.msg}>{chatMessage.message}</Text>
         </View>
       </View>
     ));
 
     return (
-      <View style={{position: 'absolute', marginTop: Dimensions.get('screen').height * 0.5}}>
+      <View style={styles.abs}>
       <View>
       <ScrollView 
       directionalLockEnabled={false}
-      contentContainerStyle={{flexDirection: 'column-reverse', marginLeft: Dimensions.get('screen').width * 0.035, maxHeight: Dimensions.get('screen').height * 0.25}}>  
+      contentContainerStyle={styles.scroll}>  
         {this.state.chatOn ? chatMessages : null}
       </ScrollView>
       </View>
-      <View style={{flexDirection: 'row', position: "absolute", marginTop: Dimensions.get('screen').height * 0.3, marginLeft: Dimensions.get('screen').width * 0.035}}>
+      <View style={styles.viewInput}>
       <TextInput
-          style={{ height: 40, width: 300, backgroundColor: 'white', borderRadius: '22/2' }}
+          style={{ height: 40, width: 300, backgroundColor: 'white', borderRadius: '22/2', paddingLeft: Dimensions.get('screen').width * 0.02 }}
           placeholder={'  Comment...'}
           autoCorrect={false}
           value={this.state.chatMessage}
@@ -62,19 +63,12 @@ export default class Social extends Component {
           }}
         />
         {this.state.chatOn ? 
-        <Text style={{fontSize: 14, color: 'black', fontWeight: '800', margin: 10}} onPress={() => this.setState({chatOn: !this.state.chatOn})}>HIDE</Text>
+        <Text style={styles.hide} onPress={() => this.setState({chatOn: !this.state.chatOn})}>HIDE</Text>
         :
-        <Text style={{fontSize: 14, color: 'black', fontWeight: '800', margin: 10}} onPress={() => this.setState({chatOn: !this.state.chatOn})}>SHOW</Text>
+        <Text style={styles.hide} onPress={() => this.setState({chatOn: !this.state.chatOn})}>SHOW</Text>
         }
       </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F5FCFF"
-  }
-});
---
+++ src/components/05_Pages/Social/Social.styling.js
@@ -0,0 +1,43 @@
+import {StyleSheet, Dimensions} from 'react-native';
+import {colors, dimensions} from '../../../settings/all_settings'
+
+const styles = StyleSheet.create({
+    proPic: {
+        width: 40, 
+        height: 40,
+        borderRadius: 40/2
+    },
+    usn: {
+        fontSize: 14, 
+        color: 'black', 
+        fontWeight: '700'
+    },
+    msg: {
+        fontSize: 14, 
+        color: 'black',
+        fontWeight: '600'
+    },
+    abs: {
+        position: 'absolute', 
+        marginTop: Dimensions.get('screen').height * 0.5
+    },
+    scroll: {
+        flexDirection: 'column-reverse', 
+        marginLeft: Dimensions.get('screen').width * 0.035, 
+        maxHeight: Dimensions.get('screen').height * 0.25
+    },
+    viewInput: {
+        flexDirection: 'row', 
+        position: "absolute", 
+        marginTop: Dimensions.get('screen').height * 0.3, 
+        marginLeft: Dimensions.get('screen').width * 0.035
+    },
+    hide: {
+        fontSize: 14, 
+        color: 'black', 
+        fontWeight: '800', 
+        margin: 10
+    }
+});
+
+export {styles};
