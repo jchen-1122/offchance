88f68af2a447f1d6318637e6f5fbbc559deba909
Work <rochellecompendio2021@u.northwestern.edu>
2020-08-03 17:40:59 -0500

    backend done

--- src/functions/explore_functions.js
+++ src/functions/explore_functions.js
@@ -1,7 +1,8 @@
+var moment = require('moment');
 const ip = require('../components/IP_ADDRESS.json')
 
 // gets array of user raffle objects, returns array of top 5 donors (user objects)
-export function top5_raffle(users){
+export function top5_raffle(users) {
     const ip = require('../components/IP_ADDRESS.json')
 
     // gets actual user object based on their ID
@@ -12,11 +13,11 @@ export function top5_raffle(users){
     }
 
     // gets prof pics of top 5 users and returns it
-    const getTop5 = async(users) => {
+    const getTop5 = async (users) => {
         var top5 = []
-        var sorted_users = users.sort((a,b)=>b.amountDonated - a.amountDonated).slice(0,5) // sort by amount donated
+        var sorted_users = users.sort((a, b) => b.amountDonated - a.amountDonated).slice(0, 5) // sort by amount donated
 
-        for (let i = 0; i<sorted_users.length; i++){
+        for (let i = 0; i < sorted_users.length; i++) {
             let user = await getUser(sorted_users[i].userID)
             top5.push(user.profilePicture)
         }
@@ -24,18 +25,20 @@ export function top5_raffle(users){
     }
 
     // driver code
-    if (users){
+    if (users) {
         return getTop5(users)
     }
 }
+
 
 // GLOBAL TOP 5 DONORS =================================================================================================================
 
 // check if a raffle is starting this week 
 function isRecentRaffle(raffle) {
     var thisWeek = moment(raffle.lastDonatedTo * 1000).isSame(new Date(), 'week');
-    var upcoming = moment(raffle.lastDonatedTo * 1000).isAfter(new Date()) // make sure it hasn't already happened
-    return thisWeek && upcoming
+    // var upcoming = moment(raffle.lastDonatedTo * 1000).isAfter(new Date()) // make sure it hasn't already happened
+    // return thisWeek && upcoming
+    return thisWeek
 }
 
 // get all the raffles happening this week
@@ -46,7 +49,57 @@ async function getRecentRaffles() {
     return response
 }
 
+// array of 5 ID's -> array of 5 user objects
+async function getDonorObjs(top5_IDs) {
+    // JSON-ify the id array
+    let data = {
+        ids: top5_IDs
+    }
+    data = JSON.stringify(data)
+
+    // make API call with multiple IDs
+    const donorRes = await fetch('http://' + ip.ipAddress + '/user/ids/', {
+        method: "PATCH",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: data
+    })
+    let res = await donorRes.json()
+    return res
+}
+
+// returns top 5 donors (user objects) this week
 export async function top5_global(users) {
     let recentRaffles = await getRecentRaffles() // all raffles this week
-    console.log(recentRaffles.length)
+    let userAmtMap = new Map() // maps id of user -> amt donated for that week
+
+    // cycle through each raffle
+    for (var raffle of recentRaffles) {
+        var enteredUsers = raffle.users.children
+
+        // cycle through each entered user in raffle
+        for (var user of enteredUsers) {
+            // if user is not in the mapping yet
+            if (!userAmtMap.has(user.userID)) {
+                userAmtMap.set(user.userID, user.amountDonated) // map user id -> amount donated for that raffle
+            }
+            // if user is already in mapping (have already donated to another raffle)
+            else {
+                let tempAmt = userAmtMap.get(user.userID)
+                userAmtMap.set(user.userID, tempAmt + user.amountDonated) // update total amount they've donated
+            }
+        }
+    }
+
+    // sort by amount donated
+    const userAmtMapSorted = new Map([...userAmtMap.entries()].sort((a, b) => b[1] - a[1]));
+
+    // convert map keys to an array
+    let top5_IDs = [...userAmtMapSorted.keys()];
+    top5_IDs = top5_IDs.slice(0, 5)
+
+    // return array of 5 user objs
+    return getDonorObjs(top5_IDs)
 }
